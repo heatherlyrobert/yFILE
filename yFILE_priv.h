b559f0d43ef99076a8947e2584fe375f19d4a5f6
heatherlyrobert <heatherlyrobert@gmail.com>
2021-12-07 21:10:47 -0800

    2.0a : first break out of functionality

---
+++ yFILE_priv.h
@@ -0,0 +1,78 @@
+/*============================----beg-of-source---============================*/
+#ifndef yFILE_PRIV
+#define yFILE_PRIV yes
+
+
+
+/*===[[ BEG_HEADER ]]=========================================================*/
+/*345678901-12345678901-12345678901-12345678901-12345678901-12345678901-12345678901-12345678901-*/
+
+/*===[[ ONE_LINERS ]]=========================================================*/
+
+#define     P_FOCUS     "RS (run-time support)"
+#define     P_NICHE     "us (user control)"
+#define     P_SUBJECT   "file management"
+#define     P_PURPOSE   ""
+
+#define     P_NAMESAKE  "apollodorus-bibliothikarios (librarian)"
+#define     P_HERITAGE  "librarian of alexandria and author"
+#define     P_IMAGERY   ""
+#define     P_REASON    ""
+
+#define     P_ONELINE   P_NAMESAKE " " P_SUBJECT
+
+#define     P_BASENAME  "yFILE"
+#define     P_FULLNAME  "/usr/local/lib64/libyFILE"
+#define     P_SUFFIX    ""
+#define     P_CONTENT   ""
+
+#define     P_SYSTEM    "gnu/linux   (powerful, ubiquitous, technical, and hackable)"
+#define     P_LANGUAGE  "ansi-c      (wicked, limitless, universal, and everlasting)"
+#define     P_CODESIZE  "large       (appoximately 10,000 slocl)"
+#define     P_DEPENDS   "none"
+
+#define     P_AUTHOR    "heatherlyrobert"
+#define     P_CREATED   ""
+
+#define     P_VERMAJOR  "2.--, clean, improve, and expand"
+#define     P_VERMINOR  "2.0-, break away from yVIKEYS"
+#define     P_VERNUM    "2.0a"
+#define     P_VERTXT    "first break out of functionality"
+
+#define     P_PRIORITY  "direct, simple, brief, vigorous, and lucid (h.w. fowler)"
+#define     P_PRINCIPAL "[grow a set] and build your wings on the way down (r. bradbury)"
+#define     P_REMINDER  "there are many better options, but i *own* every byte of this one"
+
+/*===[[ END_HEADER ]]=========================================================*/
+
+
+/*---(ansi-c standard)-------------------*/
+#include    <stdio.h>             /* clibc  standard input/output             */
+#include    <stdlib.h>            /* clibc  standard general purpose          */
+#include    <string.h>            /* clibc  standard string handling          */
+/*---(custom core)-----------------------*/
+#include    <yURG.h>              /* heatherly urgent processing              */
+#include    <yLOG.h>              /* heatherly program logging                */
+#include    <ySTR.h>              /* heatherly string processing              */
+/*---(custom vikeys)---------------------*/
+#include    <yMODE.h>             /* heatherly vi/vim mode processing         */
+#include    <yKEYS.h>             /* heatherly vi/vim mode processing         */
+/*---(custom other)----------------------*/
+#include    <yPARSE.h>            /* heatherly file reading and writing       */
+
+
+
+typedef    struct    cMY    tMY;
+struct cMY {
+   /*---(done)-----------------*/
+};
+extern tMY         myFILE;
+
+
+/*===[[ yMACRO_script.c ]]====================================================*/
+/*345678901-12345678901-12345678901-12345678901-12345678901-12345678901-123456*/
+
+
+
+
+#endif
