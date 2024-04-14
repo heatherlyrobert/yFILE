326ebb670d15e275492a36bfe63b73e443995dbd
heatherlyrobert <jelloshrike@gmail.com>
2024-04-14 04:41:23 -0700

    2.0j : minor changes for library changes

--- yFILE_priv.h
+++ yFILE_priv.h
@@ -36,8 +36,8 @@
 
 #define     P_VERMAJOR  "2.--, clean, improve, and expand"
 #define     P_VERMINOR  "2.0-, break away from yVIKEYS"
-#define     P_VERNUM    "2.0i"
-#define     P_VERTXT    "switched EXIM location to YSTR_CLIP for standardization"
+#define     P_VERNUM    "2.0j"
+#define     P_VERTXT    "minor changes for library changes"
 
 #define     P_PRIORITY  "direct, simple, brief, vigorous, and lucid (h.w. fowler)"
 #define     P_PRINCIPAL "[grow a set] and build your wings on the way down (r. bradbury)"
